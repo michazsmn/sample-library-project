3f1dd3d99471e0fd292e4e5e5903b2121d927b85
Michael <michael.zusmanovskiy@gmail.com>
2025-05-29 00:53:27 +0200

    layout changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
-import type { Metadata } from "next";
+import type { Metadata} from "next";
+import Link from "next/link";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
@@ -24,10 +25,21 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >{children}
+    <html lang="en" suppressHydrationWarning className="h-full">
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased h-full flex flex-col justify-between font-[family-name:var(--font-geist-sans)] min-h-screen`}>
+        <header className="flex justify-between p-10 bg-black">
+          <h1 className="text-5xl font-bold">Sample Library</h1>
+          <nav className="flex gap-4 text-xl">
+            <Link href="/login" className="headerLink">Login</Link>
+            <Link href="/" className="headerLink">Home</Link>
+          </nav>
+        </header>
+        <main className="flex flex-col gap-[32px] items-center">
+          {children}
+        </main>
+        <footer className="h-6 bg-black">
+
+        </footer>          
       </body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,31 +1,18 @@
-import { Button } from "@/components/ui/button"
-
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      
-      <header className="row-start-1 flex items-center justify-between w-full">
-        <h1 className="text-4xl font-bold">Sample Library</h1>
-        <nav className="flex gap-4 text-xl">
-          <a href="#" >Login</a>
-          <a href="#" >Home</a>
-        </nav>
-      </header>
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <h2 className="text-3xl font-bold">Modern Sample Library for Producers and Audio Enthusiasts</h2>
+      <div className="flex flex-col gap-[32px] items-center mb-40">
+        <h2 className="text-4xl font-bold">Modern Sample Library for Producers and Audio Enthusiasts</h2>
         <div className="flex flex-row gap-[16px] items-center justify-center w-full">
-          <Button className="bg-gray-900 hover:bg-gray-800 text-white font-bold py-2 px-4 rounded" size={"lg"}>
+          <Link href="/explore" className="buttonHomePage">
             Explore
-          </Button>
-          <Button className="bg-gray-900 hover:bg-gray-800 text-white font-bold py-2 px-4 rounded" size={"lg"}>
+          </Link>
+          <Link href="/login" className="buttonHomePage">
             Login
-          </Button>
+          </Link>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        
-      </footer>
-    </div>
+      </div>
+
   );
 }
